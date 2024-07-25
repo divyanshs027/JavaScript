f809c606cea7d84ec973dc78a651d231394471fc
Divyansh singh <95603469+divyanshs027@users.noreply.github.com>
2024-07-25 08:25:20 +0000

    project 2 solution

--- 07_Project/02_BMI_Calcu/bmi.js
+++ 07_Project/02_BMI_Calcu/bmi.js
@@ -9,6 +9,7 @@ form.addEventListener('submit', function (e) {
   const height = parseInt(document.querySelector('#height').value);
   const weight = parseInt(document.querySelector('#weight').value);
   const results = document.querySelector('#results');
+  const guide = document.querySelector('#guide');
 
   if (height === '' || height < 0 || isNaN(height)) {
     results.innerHTML = 'Please give a valid height';
@@ -18,5 +19,28 @@ form.addEventListener('submit', function (e) {
     bmi = (weight / ((height * height) / 10000)).toFixed(2);
     //show the result
     results.innerHTML = `<span>${bmi}</span>`;
+
+    if (bmi <= 18.6) {
+      guide.style.color = 'white';
+      guide.style.fontSize = '35px';
+      guide.style.marginLeft = '90px';
+      guide.style.marginTop = '20px';
+
+      guide.innerHTML = 'Under Weight';
+    } else if (bmi < 24.9) {
+      guide.style.color = 'white';
+      guide.style.fontSize = '35px';
+      guide.style.marginLeft = '90px';
+      guide.style.marginTop = '20px';
+
+      guide.innerHTML = 'Normal Weight';
+    } else {
+      guide.style.color = 'white';
+      guide.style.fontSize = '35px';
+      guide.style.marginLeft = '90px';
+      guide.style.marginTop = '20px';
+
+      guide.innerHTML = 'Over Weight';
+    }
   }
 });
